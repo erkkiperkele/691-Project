16cf82090134dca635549d610d72611ba6fe988d
erkkiperkele <aymeric.grail@gmail.com>
2016-02-28 12:43:41 -0500

    Keeping on connecting

--- DAL/dataService.py
+++ DAL/dataService.py
@@ -1,24 +1,44 @@
-# cd /Users/Aymeric/apache-drill-1.5.0
-# bin/drill-embedded
-
-
 from pydrill.client import PyDrill
 
-from DAL import queryDictionary
+from DAL import QueryDictionary
 
-drill = PyDrill(host='localhost')
-is_drill_active = drill.is_active()
 
-if is_drill_active:
-    print("Drill is active: %s" % is_drill_active)
-else:
-    print('''
-Drill is not active. Start your server in a terminal using command:
-bin/drill-embedded
-    ''')
+class DataService:
+    __drill = None
+    __queryDictionary = QueryDictionary.QueryDictionary
 
-yelp_elite = queryDictionary.get_yelp_elite()
-yelp_reviews = drill.query(yelp_elite)
+    def init_drill_connection(self):
+        self.__drill = PyDrill(host='localhost')
+        is_drill_active = self.__drill.is_active()
 
-for result in yelp_reviews:
-    print("%s - %s" % (result['user_id'], result['elite']))
+        if is_drill_active:
+            print("Drill is active: %s" % is_drill_active)
+        else:
+            print('''
+            Drill is not active. Start your server in a terminal using command:
+            cd /Users/Aymeric/apache-drill-1.5.0
+            bin/drill-embedded
+                ''')
+
+    def get_yelp_elite(self):
+        yelp_elite_query = self.__queryDictionary.get_yelp_elite_query()
+        yelp_elite = self.__drill.query(yelp_elite_query)
+        self.print_header("elite members")
+        for result in yelp_elite:
+            print("%s - %s" % (result['user_id'], result['elite']))
+
+    @staticmethod
+    def print_header(query_name):
+        print('''
+-----------------------
+Printing %s:
+-----------------------''' % query_name)
+
+    def main(self):
+        self.init_drill_connection()
+        self.get_yelp_elite()
+
+
+if __name__ == '__main__':
+    print('''Testing connection to Drill''')
+    DataService().main()
--- DAL/queryDictionary.py
+++ DAL/queryDictionary.py
@@ -1,26 +1,20 @@
-def get_yelp_test():
-    yelp_reviews = '''
-      SELECT * FROM
-      `dfs.root`.`./Users/Aymeric/apache-drill-1.5.0/YelpDataSet/yelp_academic_dataset_user.json`
-      LIMIT 5
-    '''
-    return yelp_reviews
+class QueryDictionary:
+
+    @staticmethod
+    def get_yelp_elite_query():
+        yelp_elite = '''
+         SELECT user_id, elite FROM
+            `dfs.root`.`./Users/Aymeric/apache-drill-1.5.0/YelpDataSet/yelp_academic_dataset_user.json`
+         WHERE elite[0] IS NOT NULL  LIMIT 200
+        '''
+        return yelp_elite
 
 
-def get_yelp_elite():
-    yelp_elite = '''
-     SELECT user_id, elite FROM
-        `dfs.root`.`./Users/Aymeric/apache-drill-1.5.0/YelpDataSet/yelp_academic_dataset_user.json`
-     WHERE elite[0] IS NOT NULL  LIMIT 200
-    '''
-    return yelp_elite
-
-
-def get_yelp_elite_count():
-    yelp_elite_count = '''
-     SELECT count(*) FROM
-        dfs.`/Users/Aymeric/apache-drill-1.5.0/YelpDataSet/yelp_academic_dataset_user.json`
-    WHERE elite[0] IS NOT NULL  LIMIT 200
-    '''
-    return yelp_elite_count
-
+    @staticmethod
+    def get_yelp_elite_count_query():
+        yelp_elite_count = '''
+         SELECT count(*) FROM
+            dfs.`/Users/Aymeric/apache-drill-1.5.0/YelpDataSet/yelp_academic_dataset_user.json`
+        WHERE elite[0] IS NOT NULL  LIMIT 200
+        '''
+        return yelp_elite_count
