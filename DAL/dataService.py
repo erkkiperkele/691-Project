bd147a221a32727403eb6ed43c21340682cc938d
erkkiperkele <aymeric.grail@gmail.com>
2016-03-06 11:17:48 -0500

    Added a join query example. (# of review per elite member). Note that by default query on reviews timeout (set timeout to 30 sec, works on mac book air)

--- DAL/dataService.py
+++ DAL/dataService.py
@@ -21,11 +21,41 @@ class DataService:
                 ''')
 
     def get_yelp_elite(self):
-        yelp_elite_query = self.__dictionary.get_yelp_elite_query()
-        yelp_elite = self.__drill.query(yelp_elite_query)
+        query = self.__dictionary.get_elite()
+        yelp_elite = self.__drill.query(query)
+
         self.print_header("elite members")
         for result in yelp_elite:
             print("%s - %s" % (result['user_id'], result['elite']))
+
+    def get_yelp_elite_count(self):
+        query = self.__dictionary.get_elite_count()
+        yelp_elite = self.__drill.query(query)
+
+        for result in yelp_elite:
+            print("elite count: {0}".format(result['elite_count']))
+
+    def get_yelp_elite_tip(self):
+        query = self.__dictionary.get_elite_tip()
+        yelp_elite_tip = self.__drill.query(query)
+
+        self.print_header("elite extended members")
+        print("user_id \t\t\t\t tip_count")
+        for result in yelp_elite_tip:
+            print("%s \t %s" % (result['user_id'], result['tip_count']))
+
+        self.print_count(yelp_elite_tip)
+
+    def get_yelp_elite_review(self):
+        query = self.__dictionary.get_elite_review()
+        yelp_elite_review = self.__drill.query(query, 30)
+
+        self.print_header("elite extended members")
+        print("user_id \t\t\t\t review_count")
+        for result in yelp_elite_review:
+            print("%s \t %s" % (result['user_id'], result['review_count']))
+
+        self.print_count(yelp_elite_review)
 
     @staticmethod
     def print_header(query_name):
@@ -34,9 +64,16 @@ class DataService:
 Printing %s:
 -----------------------''' % query_name)
 
+    @staticmethod
+    def print_count(records):
+        print("Number of records: {0}".format(len(records.rows)))
+
     def main(self):
         self.init_drill_connection()
-        self.get_yelp_elite()
+        # self.get_yelp_elite()
+        # self.get_yelp_elite_count()
+        # self.get_yelp_elite_tip()
+        self.get_yelp_elite_review()
 
 
 if __name__ == '__main__':
--- DAL/queryDictionary.py
+++ DAL/queryDictionary.py
@@ -5,27 +5,53 @@ class QueryDictionary:
     """
     __dataPath = '''`dfs.root`.`./Users/Aymeric/apache-drill-1.5.0/YelpDataSet/'''
 
-    __user = {'user': __dataPath + '''yelp_academic_dataset_user.json`'''}
-    __business = {'business': __dataPath + '''yelp_academic_dataset_business.json`'''}
-    __checkin = {'checkin': __dataPath + '''yelp_academic_dataset_checkin.json`'''}
-    __review = {'review': __dataPath + '''yelp_academic_dataset_review.json`'''}
-    __tip = {'tip': __dataPath + '''yelp_academic_dataset_tip.json`'''}
+    __tables = {
+        'user': __dataPath + '''yelp_academic_dataset_user.json`''',
+        'business': __dataPath + '''yelp_academic_dataset_business.json`''',
+        'checkin': __dataPath + '''yelp_academic_dataset_checkin.json`''',
+        'review': __dataPath + '''yelp_academic_dataset_review.json`''',
+        'tip': __dataPath + '''yelp_academic_dataset_tip.json`'''
+    }
 
     @classmethod
-    def get_yelp_elite_query(cls):
+    def get_elite(cls):
         yelp_elite = '''
-         SELECT user_id, elite FROM
-            {user}
-         WHERE elite[0] IS NOT NULL  LIMIT 200
-        '''.format(**cls.__user)
+                 SELECT user_id, elite FROM {user}
+                 WHERE elite[0] IS NOT NULL  LIMIT 200
+        '''.format(**cls.__tables)
 
         return yelp_elite
 
     @classmethod
-    def get_yelp_elite_count_query(cls):
+    def get_elite_count(cls):
         yelp_elite_count = '''
-         SELECT count(*) FROM
-            {user}
-        WHERE elite[0] IS NOT NULL  LIMIT 200
-        '''.format(cls.__user)
+                SELECT count(*) as elite_count FROM {user}
+                WHERE elite[0] IS NOT NULL
+        '''.format(**cls.__tables)
         return yelp_elite_count
+
+    @classmethod
+    def get_elite_tip(cls):
+
+        yelp_elite = '''
+                SELECT u.user_id, count(*) as tip_count  FROM {user} u
+                   JOIN {tip} as t
+                       on u.user_id = t.user_id
+                WHERE u.elite[0] IS NOT NULL
+                GROUP BY u.user_id
+                LIMIT 200
+        '''.format(**cls.__tables)
+        return yelp_elite
+
+    @classmethod
+    def get_elite_review(cls):
+
+        yelp_elite = '''
+                SELECT u.user_id, count(*) as review_count FROM {user} u
+                   JOIN {review} as r
+                       on u.user_id = r.user_id
+                WHERE u.elite[0] IS NOT NULL
+                GROUP BY u.user_id
+                LIMIT 50000
+        '''.format(**cls.__tables)
+        return yelp_elite
